1eed6db17cfffe3166178f9cc41052a3c9093a8d
sokjunem <skim23@babson.edu>
2016-09-06 18:56:31 -0400

    9.5 Class

---
+++ demo.py
@@ -0,0 +1,8 @@
+message = 'Please enter your name'
+print(message)
+name = input()
+print('Hello,', name, 'Nice to meet you')
+
+
+input()
+
--- hello.py
+++ hello.py
@@ -1,1 +1,12 @@
-print('Hello, world')
+message = 'Please enter your name: '
+print(message)
+name = input()
+print('Hello,', name)
+
+
+
+input()
+'Today is %2d-%02d.' % (9, 6)
+'Pi equals %.2f.' % 3.1415926
+'Age: %s. Gender: %s' % (20, True)
+'Growth rate: %d %%' % 7
