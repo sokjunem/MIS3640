dd6e5e0dc759ffc1e5ed4f4e6a20ed970f358d2b
sokjunem <skim23@babson.edu>
2016-10-28 22:17:36 -0400

    change

--- Assignment/Assignment3/Assignment3_db.py
+++ Assignment/Assignment3/Assignment3_db.py
@@ -11,9 +11,9 @@ for i in range(10):
 
 
 
-# file_title = 'review_combined.txt'
-# file = open(file_title, 'w', encoding='utf8')
-# for i in range(21):
-#     file.write(imdb._get_reviews_data('tt1457767')[i]['text'])
-#     file.write('\n\n\n')
-# file.close()
+file_title = 'review_combined.txt'
+file = open(file_title, 'w', encoding='utf8')
+for i in range(21):
+    file.write(imdb._get_reviews_data('tt1457767')[i]['text'])
+    file.write('\n\n\n')
+file.close()
